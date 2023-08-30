0263031081044835d1d9c630a68f28c5272693b0
worotyns <mateusz@pushpushgo.com>
2023-08-30 09:47:07 +0200

    chore: package.json

--- emojis/generate.sh
+++ emojis/generate.sh
@@ -1,7 +1,7 @@
-rm -f emoji.json
-rm -f sctuc.json
-rm -f ../src/front/sctuc.json
+rm -f emojis/emoji.json
+rm -f emojis/sctuc.json
+rm -f src/front/sctuc.json
 
-wget https://raw.githubusercontent.com/iamcal/emoji-data/master/emoji.json
-node convert.js
-mv sctuc.json ../src/front/sctuc.json
+wget https://raw.githubusercontent.com/iamcal/emoji-data/master/emoji.json -O emojis/emoji.json
+node emojis/convert.js
+mv emojis/sctuc.json src/front/sctuc.json
