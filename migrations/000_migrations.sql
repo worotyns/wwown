2216da286f0de3b01145f8dccfa70ef30a2651b6
worotyns <mateusz@pushpushgo.com>
2024-01-05 08:24:13 +0100

    fix: replace old version of code to new code

--- migrations/000_migrations.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS migrations (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    migration_name TEXT,
-    applied_at DATETIME
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS migration_idx ON migrations(migration_name);
--- migrations/005_create_karma_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS karma (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    channel_id VARCHAR(32),
-    reacting_user_id VARCHAR(32),
-    user_id VARCHAR(32),
-    timestamp TIMESTAMP,
-    reaction TEXT
-);
